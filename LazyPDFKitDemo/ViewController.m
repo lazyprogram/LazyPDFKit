567a16a2f5b254ea644d118e8af70618dcc75c02
Palanisamy Easwaramoorthy <ctepv1@biitd-ctepv1881.users.sphinet.com.sg>
2015-03-16 14:42:42 +0800

    Updated

---
+++ LazyPDFKitDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LazyPDFKitDemo
+//
+//  Created by Palanisamy Easwaramoorthy on 16/3/15.
+//  Copyright (c) 2015 Lazyprogram. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
